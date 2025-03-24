e6a084f51450184757a133853dc0e44c3cd12eb5
Raviteja Narayanam <quic_ravnar@quicinc.com>
2025-03-24 14:48:56 +0530

    kgsl: Update syntax for MODULE_IMPORT_NS usage
    Update MODULE_IMPORT_NS syntax for kernel version 6.13 and later.
    
    Change-Id: I3f8e940c3a3ceb7b4af0649e3581c6001c006095
    Signed-off-by: Raviteja Narayanam <quic_ravnar@quicinc.com>

--- adreno.c
+++ adreno.c
@@ -3725,6 +3725,8 @@ module_exit(kgsl_3d_exit);
 MODULE_DESCRIPTION("3D Graphics driver");
 MODULE_LICENSE("GPL v2");
 MODULE_SOFTDEP("pre: arm_smmu nvmem_qfprom socinfo");
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0))
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 13, 0)
+MODULE_IMPORT_NS("DMA_BUF");
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0)
 MODULE_IMPORT_NS(DMA_BUF);
 #endif
